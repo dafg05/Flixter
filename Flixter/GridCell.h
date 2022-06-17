977353b4c9008e83503b31d2c66823a27b7cd612
dafg05 <danialefloresg@gmail.com>
2022-06-17 11:00:33 -0700

    Add collection view

---
+++ Flixter/GridCell.h
@@ -0,0 +1,16 @@
+//
+//  GridCell.h
+//  Flixter
+//
+//  Created by Daniel Flores Garcia on 6/17/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GridCell : UICollectionViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Flixter/GridCell.m
@@ -0,0 +1,12 @@
+//
+//  GridCell.m
+//  Flixter
+//
+//  Created by Daniel Flores Garcia on 6/17/22.
+//
+
+#import "GridCell.h"
+
+@implementation GridCell
+
+@end
